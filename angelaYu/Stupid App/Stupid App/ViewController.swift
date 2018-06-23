cb47af38a27acd133a1e86513de791ceb2343910
Shaun <shaun.curiosit@gmail.com>
2018-06-23 12:22:50 -0500

    Angela Yu files
    initial upload

---
+++ angelaYu/Stupid App/Stupid App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stupid App
+//
+//  Created by Shaun on 5/27/18.
+//  Copyright © 2018 CuriosIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- basics/poker.playground/Contents.swift
+++
@@ -1,8 +0,0 @@
-//: Playground - noun: a place where people can play
-
-import UIKit
-
-var hearts = [
-    "King": 8,
-    "Queen"
-]
